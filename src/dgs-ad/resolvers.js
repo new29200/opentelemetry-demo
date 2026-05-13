e985ac03ff1bfde34ac4a644963bf029a5feca71
Johan <johannedpro@gmail.com>
2026-05-13 11:23:47 +0200

    Ajout du service dgs-ad

---
+++ src/dgs-ad/resolvers.js
@@ -0,0 +1,40 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+let adClient;
+
+const initGrpcClient = (adServiceAddr) => {
+  if (adClient) return;
+
+  const packageDefinition = protoLoader.loadSync('../../../pb/demo.proto', {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+
+  const proto = grpc.loadPackageDefinition(packageDefinition);
+  const AdService = proto.oteldemo.AdService;
+  adClient = new AdService(adServiceAddr, grpc.credentials.createInsecure());
+};
+
+const resolvers = {
+  Query: {
+    getAds: async (_, { contextKeys }, context) => {
+      initGrpcClient(context.adServiceAddr);
+
+      return new Promise((resolve, reject) => {
+        adClient.getAds({ context_keys: contextKeys }, (err, response) => {
+          if (err) reject(err);
+          else resolve(response.ads);
+        });
+      });
+    },
+  },
+};
+
+module.exports = resolvers;
