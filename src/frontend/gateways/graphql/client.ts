14efedd55b269dea64b816cc6ead771f38c0332b
Johan <johannedpro@gmail.com>
2026-05-20 14:15:43 +0200

    fix port front end dans le env

--- src/frontend/gateways/graphql/client.ts
+++ src/frontend/gateways/graphql/client.ts
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000',  
+    uri: `http://localhost:${process.env.NEXT_PUBLIC_GRAPHQL_PORT}`, 
     credentials: 'include',
   }),
   cache: new InMemoryCache(),
